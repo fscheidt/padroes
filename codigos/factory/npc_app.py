aafdaae2ff86c037054052cab673924844e21df0
fscheidt <felippescheidt@gmail.com>
2023-08-02 20:52:39 -0300

    update: npc_app.py

--- codigos/factory/npc_app.py
+++ codigos/factory/npc_app.py
@@ -38,5 +38,20 @@ if __name__ == "__main__":
   inimigo = NpcFactory.create(NpcType.BOWSE)
   # (2) cria uma instancia de jogador:
   mario = Player("Mario", 100)
-  
+  # (3) inimigo ataca Player e atualizar HP
   inimigo.atacar(mario)
+  # (4) exibir HP do player
+
+"""
+Atividade 1:
+a) Implementar o método Ataque da Classe NPC
+- Dragon, força de ataque=15
+- Bowse, força ataque=10
+
+b) Implementar o Factory da Classe Player:
+- Mario, hp=100, defesa=8
+- Joker, hp=120, defesa=5
+
+c) Gerar 2 inimigos usando o NPCFactory, para atacar 1 player. Exibir o HP do player.
+
+"""
