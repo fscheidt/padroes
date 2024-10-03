5ca5e9d446003ac0b7c521eee7058c0e1b5ce6f7
Felippe Scheidt <felippescheidt@gmail.com>
2024-10-02 22:08:51 -0300

    aula 1

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+## Slides
+
+- [Aula 1](https://docs.google.com/presentation/d/1CcVJ_LM3qVAEoyoOP34-ic-l42zTk5YAXqMAB77xDG8/edit?usp=sharing)
+
 ## Virtual env
 
 Instalar virtual env sem permissão de superusuário:
--- main.py
+++ main.py
@@ -1,10 +1,27 @@
 from abc import ABC
-class Pagamento(ABC):
-    ...
-    
-class PagamentoPix(Pagamento):
-    ...
+class Pagamento(ABC):  # classe abstrata
+    def __init__(self, valor):  # metodo construtor
+        # atributo, inicializado na criacao do objeto  
+        self.valor = valor if valor > 0 else 0  
+    def status(self):
+        print(f"valor pagamento: {self.valor:.2f}")
+
+class PagamentoPix(Pagamento): # Herda de Pagamento
+    def __init__(self, valor):
+        super().__init__(valor)  # super eh a classe Pagamento
+
+class PagamentoCredito(Pagamento): # Herda de Pagamento
+    def __init__(self, valor):
+        super().__init__(valor) 
+        self.valor = self.valor * 1.1  # adiciona acrescimo de 10%
 
 if __name__ == "__main__":
-    pag = Pagamento()
+    pag = PagamentoPix(100)
+    pag.status()
+    
+    pag = PagamentoCredito(100)
+    pag.status()
 
+    pag = PagamentoPix(-100)
+    pag.status()
+    
