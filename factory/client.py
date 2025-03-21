c44fe6f0879181bccf3fcff9398488770f1d6a5f
Felippe Scheidt <felippescheidt@gmail.com>
2025-03-21 20:41:50 -0300

    Aula 2 - Factory method

--- factory/client.py
+++ factory/client.py
@@ -1,5 +1,8 @@
-# from image_factory import create_image, ImageType
-from models import Image, JPG, PNG 
+# from models import Image, JPG, PNG 
+from image_factory import (
+    create_image, 
+    ImageType
+)
 
 if __name__ == "__main__":
     ### CLASSE CLIENTE (1 of 50) ###
@@ -8,9 +11,24 @@ if __name__ == "__main__":
     #   (b) formato para exportação 
     image_path = "/tmp/image.jpg"
     export_type = ImageType.PNG
+    
+    # -------------------------------
+    # SEM factory:
+    # -------------------------------
+    # image.export(image_path)
+    # image = PNG(channels=16)
+    # image.export(image_path)
+    
+    # ==============================
+    # USANDO Factory:
+    # ==============================
     # Cliente desconhece quais são as classes concretas
     # de imagens disponíveis, portanto pede ao factory
-    # pela instância delegando a responsabilidade a este
-    image = create_image(export_type)
+    # pela instância delegando a responsabilidade a este.
+    # -------------------------------
+    image = create_image(export_type)  # <= solicita imagem PNG
     image.export(image_path, rgba=True)
-
+    
+    # -------------------------------
+    image = create_image(ImageType.JPG) # <= solicita imagem JPG
+    image.export("/tmp/profile.gif")  
--- factory/image_factory.py
+++ factory/image_factory.py
@@ -7,6 +7,13 @@ class ImageType(Enum):
     PNG = "png"
     JPG = "jpg"
 
-def create_image():
+def create_image(image_type: ImageType) -> Image:
     """ Factory method """
-    raise NotImplementedError("not implemented")
+    if ImageType.PNG.value == image_type.value:
+        # instanciar uma imagem do tipo PNG ...
+        return PNG()
+    elif ImageType.JPG.value == image_type.value:
+        return JPG(0.9)
+    else:
+        raise ValueError(f"Unknown image type: {image_type}")
+    
