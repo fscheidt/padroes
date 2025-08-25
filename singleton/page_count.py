5c8b2694a9884c4b9e0024774b16ff4d05598c49
Felippe Scheidt <felippescheidt@gmail.com>
2025-08-25 20:26:13 -0300

    Aula 4 - Singleton

--- singleton/page_count.py
+++ singleton/page_count.py
@@ -1,19 +1,43 @@
 class PageViews:
+    """ singleton que mantem o contador de acesso """
+    _instance = None  # representa um objeto da classe PageViews
 
     def __init__(self):
-        self.counter = 0
+        """ método construtor """
+        print("__init__")
+        if not hasattr(self, "counter"):
+            self.counter = 0
+
+    @staticmethod
+    def get_instance():
+        """ ponto de acesso global """
+        return PageViews()
         
     def add_visiter(self):
         """ incrementa contador de visitantes """
         self.counter += 1
-        print(self.counter)
+        print(f"{self.counter=}")
 
-    # def __new__(cls):
-    #     if cls._instance is None:
-    #         cls._instance = super().__new__(cls)
-    #     return cls._instance
+    def __new__(cls): # cls == PageViews
+        """ sobrescreve o metodo __new__ """
+        print("__new__")
+        if cls._instance is None:
+            # cria instancia:
+            print("creating _instance")
+            cls._instance = super().__new__(cls) 
+        return cls._instance
 
 
 if __name__ == "__main__":
-    page = PageViews()  
-    
+    page = PageViews()  #  page = PageViews.__init__()
+    page.add_visiter()
+    page.add_visiter()
+    print(id(page)) # endereco de memoria
+
+    page2 = PageViews.get_instance()
+    page2.add_visiter()
+    print(id(page2)) # endereco de memoria
+
+    page3 = PageViews.get_instance()
+    page3.add_visiter()
+    print(id(page3)) # endereco de memoria
