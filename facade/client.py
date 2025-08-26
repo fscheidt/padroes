a2a0f8d76de58fcaeace437a9e5592a17e339fbf
Felippe Scheidt <felippescheidt@gmail.com>
2025-08-25 21:47:36 -0300

    Aula 4 - Facade

--- facade/client.py
+++ facade/client.py
@@ -1,7 +1,14 @@
-from aereo import ReservaVoo
-from hotels import ReservaHotel
-from transporte import ReservaTransporte
+from pacotes import PacoteFacade
 
 if __name__ == "__main__":
     """ Interface "terminal" para reserva de pacotes """
-    pass
+
+    pacote = PacoteFacade()
+    pacote.reservar(
+        origem="Foz do Iguaçu",
+        destino="Curitiba",
+        data="2026-10-10",
+        data_entrada="2026-10-11",
+        data_saida="2026-10-20",
+    )
+
--- facade/pacotes.py
+++ facade/pacotes.py
@@ -1,4 +1,20 @@
+from aereo import ReservaVoo
+from hotels import ReservaHotel
+from transporte import ReservaTransporte
+
 class PacoteFacade:
+    """ 
+    Fornece uma interface unificada para simplificar o processo de 
+    reserva de pacotes turísticos que envolvem aéreo, hospedagem e transporte
+    """
     def __init__(self):
-        pass
+        self.aereo = ReservaVoo()
+        self.hotels = ReservaHotel()
+        self.transporte = ReservaTransporte()
+
+    def reservar(self, origem, destino, data, data_entrada, data_saida):
+        self.aereo.reservar_voo(origem, destino, data)
+        self.hotels.reservar_hotel(destino, data_entrada, data_saida)
+        self.transporte.reservar_transporte(destino, data)
+
 
