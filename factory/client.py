bf064dbc5e413b287c9e194ccd7268d0594596db
Felippe Scheidt <felippescheidt@gmail.com>
2026-02-25 19:09:13 -0300

    aula 2 - factory

---
+++ factory/client.py
@@ -0,0 +1,10 @@
+from models import Image, JPG, PNG 
+
+if __name__ == "__main__":
+    ### CLASSE CLIENTE (1 of 50) ###
+    # Define em tempo de execução:
+    #   (a) caminho do arquivo da imagem origem
+    #   (b) formato para exportação 
+    image_path = "/tmp/image.jpg"
+
+    
